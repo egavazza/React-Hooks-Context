0811c90a438c402ec8642ab094ae662b57a3e15f
Ezequiel Gavazza <egavazza@gmail.com>
2023-05-23 13:04:06 -0300

    Update Datos Usuatio - stepper complete

--- src/Form/DatosPersonales/index.js
+++ src/Form/DatosPersonales/index.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Button, Box } from "@mui/material";
+import { validarNombre, validarApellido, validarTelefono } from "./validaciones";
 
-const DatosPersonales = () => {
+
+const DatosPersonales = ({ updateStep}) => {
+
+  const [name, setName] = useState({ value: '', valid: null })
+  const [lastName, setLastName] = useState({ value: '', valid: null })
+  const [phone, setPhone] = useState({ value: '', valid: null })
+
   return (
     <Box
       component="form"
@@ -12,6 +19,10 @@ const DatosPersonales = () => {
         justifyContent: "center",
         flexDirection: "column",
       }}
+      onSubmit= {(e) => {
+        e.preventDefault();
+        updateStep(2);
+      }}
     >
       <TextField
         label="Nombre"
@@ -19,6 +30,13 @@ const DatosPersonales = () => {
         fullWidth
         margin="dense"
         type="text"
+        value={name.value}
+        onChange = {(input) => {
+          const name = input.target.value;
+          const valid = validarNombre(name);
+          setName({value: name , valid: valid});
+          console.log (name, valid);
+        }}
       />
       <TextField
         label="Apellidos"
@@ -26,6 +44,13 @@ const DatosPersonales = () => {
         fullWidth
         margin="dense"
         type="text"
+        value={lastName.value}
+        onChange = {(input) => {
+          const lastName = input.target.value;
+          const valid = validarApellido(lastName);
+          setLastName({value: lastName , valid: valid});
+          console.log (lastName, valid);
+        }}
       />
       <TextField
         label="Número telefónico"
@@ -34,6 +59,13 @@ const DatosPersonales = () => {
         margin="dense"
         type="number"
         inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+        value={phone.value}
+        onChange = {(input) => {
+          const phone = input.target.value;
+          const valid = validarTelefono(phone);
+          setPhone({value: phone , valid: valid});
+          console.log (phone, valid);
+        }}
       />
       <Button variant="contained" type="submit">
         Siguiente
